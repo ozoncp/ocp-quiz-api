e75da7ac368fc2eb6c46b8527c745831fcbf44e1
kireev yuriy <kireev_y@tochka.com>
2021-08-24 17:41:00 +0300

    - Update Repo interface - Initial dbRepo struct - Update Deps on Repo - Regenerate mocks - fix tests

--- go.mod
+++ go.mod
@@ -3,14 +3,17 @@ module github.com/ozoncp/ocp-quiz-api
 go 1.16
 
 require (
+	github.com/Masterminds/squirrel v1.5.0
 	github.com/golang/mock v1.6.0
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
+	github.com/jackc/pgx/v4 v4.13.0
+	github.com/jmoiron/sqlx v1.3.4
 	github.com/onsi/ginkgo v1.16.4
 	github.com/onsi/gomega v1.10.1
 	github.com/ozoncp/ocp-quiz-api/pkg/ocp-quiz-api v0.0.0-00010101000000-000000000000
 	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.23.0
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4
 	google.golang.org/grpc v1.40.0
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -1,7 +1,11 @@
 package repo
 
 import (
+	"context"
 	"errors"
+
+	sq "github.com/Masterminds/squirrel"
+	sql "github.com/jmoiron/sqlx"
 
 	"github.com/ozoncp/ocp-quiz-api/internal/models"
 )
@@ -10,7 +14,39 @@ var ErrCannotAddEntity = errors.New("cannot add entity")
 
 // Repo - интерфейс хранилища для сущности Quiz
 type Repo interface {
-	AddEntities(entities []models.Quiz) error
-	ListEntities(limit, offset uint64) ([]models.Quiz, error)
-	DescribeEntity(entityId uint64) (*models.Quiz, error)
+	AddEntities(ctx context.Context, entities []models.Quiz) error
+	ListEntities(ctx context.Context, limit, offset uint64) ([]models.Quiz, error)
+	DescribeEntity(ctx context.Context, entityId uint64) (*models.Quiz, error)
+	RemoveEntity(ctx context.Context, entityId uint64) bool
+}
+
+type dbRepo struct {
+	stmBuilder sq.StatementBuilderType
+}
+
+func (d *dbRepo) AddEntities(ctx context.Context, entities []models.Quiz) error {
+	panic("implement me")
+}
+
+func (d *dbRepo) ListEntities(ctx context.Context, limit, offset uint64) ([]models.Quiz, error) {
+	panic("implement me")
+}
+
+func (d *dbRepo) DescribeEntity(ctx context.Context, entityId uint64) (*models.Quiz, error) {
+	panic("implement me")
+}
+
+func (d *dbRepo) RemoveEntity(ctx context.Context, entityId uint64) bool {
+	panic("implement me")
+}
+
+func newRepo(db *sql.DB) *dbRepo {
+	stmCache := sq.NewStmtCache(db)
+	builder := sq.StatementBuilder
+	builder.PlaceholderFormat(sq.Dollar).RunWith(stmCache)
+
+	return &dbRepo{
+		stmBuilder: builder,
+	}
+
 }
